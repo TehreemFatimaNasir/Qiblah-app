356fc213ac1f7c577da8a3f1abaa18133a1fff9b
tehreem fatima <tehism672@gmail.com>
2025-05-18 23:09:04 +0500

    message

---
+++ lib/main.dart
@@ -0,0 +1,127 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_qiblah/flutter_qiblah.dart';
+
+void main() {
+  runApp(const QiblaApp());
+}
+
+class QiblaApp extends StatelessWidget {
+  const QiblaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Qibla Finder',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+// ---------------- SPLASH SCREEN ----------------
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) =>  QiblaCompassScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Image(
+              image: AssetImage('images/qibla.png'),
+              width: 200,
+              height: 200,
+              color: Colors.blueGrey,
+            ),
+            SizedBox(height: 30),
+            Text(
+              'Welcome To Qibla Finder App',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// ---------------- QIBLA COMPASS SCREEN ----------------
+
+class QiblaCompassScreen extends StatefulWidget {
+  const QiblaCompassScreen({super.key});
+
+  @override
+  State<QiblaCompassScreen> createState() => _QiblaCompassScreenState();
+}
+
+class _QiblaCompassScreenState extends State<QiblaCompassScreen> {
+  double _angle = 0;
+  late StreamSubscription<QiblahDirection> _qiblahStream;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _qiblahStream = FlutterQiblah.qiblahStream.listen((direction) {
+      setState(() {
+        _angle = direction.qiblah * (-pi / 180);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _qiblahStream.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text('Qibla Compass'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Transform.rotate(
+          angle: _angle,
+          child: Image.asset(
+            'images/qibla_image.png',
+            width: 250,
+            height: 250,
+          ),
+        ),
+      ),
+    );
+  }
+}
